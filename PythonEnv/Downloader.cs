d87299163bf5c9d8359f86dd60a76a4b1908f7ee
lytree <hi.yangzh@outlook.com>
2023-01-14 11:38:56 +0800

    添加项目文件。

---
+++ PythonEnv/Downloader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Python.Deployment
+{
+    public static class Downloader
+    {
+        private static readonly HttpClient httpClient = new();
+
+        public static async Task Download(
+            string downloadUrl,
+            string outputFilePath,
+            Action<float>? progress = null,
+            CancellationToken token = default)
+        {
+            try
+            {
+                using FileStream fileStream = new(outputFilePath, FileMode.Create);
+                await httpClient.DownloadWithProgressAsync(downloadUrl, fileStream, progress, token);
+            }
+            catch
+            {
+                if (File.Exists(outputFilePath))
+                {
+                    File.Delete(outputFilePath);
+                }
+                throw;
+            }
+        }
+    }
+
+    public static class HttpClientExtension
+    {
+        public static async Task DownloadWithProgressAsync(
+            this HttpClient client,
+            string requestUri,
+            Stream destination,
+            Action<float>? progress = null,
+            CancellationToken cancellationToken = default)
+        {
+            using var response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            var contentLength = response.Content.Headers.ContentLength;
+
+            using var download = await response.Content.ReadAsStreamAsync(cancellationToken);
+            int bufferSize = 81920;
+
+            if (progress == null || !contentLength.HasValue)
+            {
+                await download.CopyToAsync(destination, bufferSize, cancellationToken);
+                return;
+            }
+
+            var buffer = new byte[bufferSize];
+            long totalBytesRead = 0;
+            int bytesRead;
+            while ((bytesRead = await download.ReadAsync(buffer, cancellationToken)) != 0)
+            {
+                await destination.WriteAsync(buffer.AsMemory(0, bytesRead), cancellationToken);
+                totalBytesRead += bytesRead;
+                var progressPercentage = ((float)totalBytesRead / contentLength.Value) * 100;
+                progress.Invoke(progressPercentage);
+            }
+        }
+    }
+}
